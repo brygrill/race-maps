0ea70d44c3a35e0f528bedae90a0522191efd1f2
Bryan Grill <grillb@gmail.com>
2017-09-30 15:09:53 -0400

    add mylocation point

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const me = process.env.REACT_APP_ME;
 
 export default class App extends Component {
   state = {
-    loc: null,
+    loc: [-76.05, 40.39],
   };
 
   componentDidMount() {
@@ -24,67 +24,123 @@ export default class App extends Component {
     // Add Controls
     map.addControl(new mapboxgl.NavigationControl());
 
-    map.addControl(new mapboxgl.GeolocateControl({
-      positionOptions: {
-        enableHighAccuracy: true,
-      },
-      trackUserLocation: true,
-    }));
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        trackUserLocation: true,
+      }),
+    );
 
     // Load Map
-    this.mapOnLoad(map);
-
-    // Get location
-    this.getMyLocation();
+    this.mapOnLoad(map).then(() => {
+      // Get location
+      this.getMyLocation(map);
+    }).catch(error => {
+      console.log(error);
+    })
   }
 
-  getMyLocation = () => {
+  getMyLocation = map => {
     const loc = fire.database().ref(`users/${me}`);
     loc.on('value', data => {
       const locData = data.val();
+      const lng = locData.location.coords.longitude;
+      const lat = locData.location.coords.latitude;
+      // can seem to find source when fires right away
+      // this is sucky but should work for now
+      setTimeout(() => {
+      map
+        .getSource('mylocation')
+        .setData(this.locationToGeoJson([lng, lat]));
+      }, 1500);
       this.setState({ loc: locData });
     });
   };
 
+  locationToGeoJson = coordinates => {
+    return {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates,
+      },
+      properties: {},
+    };
+  };
+
   mapOnLoad = map => {
-    map.on('load', () => {
-      map.addSource('bc50k', {
-        type: 'geojson',
-        data: bc50k,
-      });
+    return new Promise((resolve, reject) => {
+      try {
+        map.on('load', () => {
+          // Blues Cruise course
+          map.addSource('bc50k', {
+            type: 'geojson',
+            data: bc50k,
+          });
 
-      map.addLayer({
-        id: 'bc50k',
-        type: 'line',
-        source: 'bc50k',
-        layout: {
-          'line-join': 'round',
-          'line-cap': 'round',
-        },
-        paint: {
-          'line-color': '#1E824C',
-          'line-width': 4,
-        },
-      });
+          map.addLayer({
+            id: 'bc50k',
+            type: 'line',
+            source: 'bc50k',
+            layout: {
+              'line-join': 'round',
+              'line-cap': 'round',
+            },
+            paint: {
+              'line-color': '#1E824C',
+              'line-width': 4,
+            },
+          });
 
-      map.addSource('bc50kmarkers', {
-        type: 'geojson',
-        data: bc50kMarkers,
-      });
+          // Mile markers
+          map.addSource('bc50kmarkers', {
+            type: 'geojson',
+            data: bc50kMarkers,
+          });
 
-      map.addLayer({
-        id: 'bc50kmarkers',
-        type: 'symbol',
-        source: 'bc50kmarkers',
-        layout: {
-          'text-field': '{mark}',
-        },
-        paint: {
-          'text-color': '#fff',
-          'text-halo-color': '#000',
-          'text-halo-width': 5,
-        },
-      });
+          map.addLayer({
+            id: 'bc50kmarkers',
+            type: 'symbol',
+            source: 'bc50kmarkers',
+            layout: {
+              'text-field': '{mark}',
+            },
+            paint: {
+              'text-color': '#fff',
+              'text-halo-color': '#000',
+              'text-halo-width': 5,
+            },
+          });
+
+          // Location from firebase
+          map.addSource('mylocation', {
+            type: 'geojson',
+            data: {
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [0, 0],
+              },
+              properties: {},
+            },
+          });
+
+          map.addLayer({
+            id: 'mylocation',
+            type: 'circle',
+            source: 'mylocation',
+            paint: {
+              'circle-color': '#674172',
+              'circle-radius': 8,
+            },
+          });
+        });
+        resolve(map);
+      } catch (e) {
+        reject(e);
+      }
     });
   };
 
